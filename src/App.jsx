6cad7c922462db5390b1eca480ee122def9806cd
Trung <qtrung811203@gmail.com>
2024-09-20 16:39:39 +0700

    chore: Update npm dependencies for react-query and react-query-devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 import GlobalStyles from "./styles/GlobalStyles"
 import AppLayout from "./ui/AppLayout"
@@ -8,9 +10,19 @@ import ProductDetailPage from "./pages/ProductDetailPage"
 import PageNotFound from "./pages/NotFoundPage"
 import LoginPage from "./pages/LoginPage"
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -25,7 +37,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   )
 }
 
